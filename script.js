56b50cd5dd24dd1bf268042433a88b38a8986ddd
OPDhaker <OPDhaker2007@gmail.com>
2022-11-11 16:53:38 +0530

    Rotation update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
-let number = Math.ceil(Math.random()*360);
+let number = Math.ceil(Math.random()*10000);
 
 btn.onclick = function(){
   container.style.transform = "rotate("+ number + "deg)"
-  number += Math.ceil(Math.random()*360);
+  number += Math.ceil(Math.random()*10000);
 }
